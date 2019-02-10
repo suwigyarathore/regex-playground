718a349dbcd756c62a506f49166bbec765a6f205
Suwigya Ratrhore <suwigya.rathore.ui@gmail.com>
2019-02-10 07:07:47 +0100

    Adding checks for .  and escaping .

--- start.js
+++ start.js
@@ -2,21 +2,25 @@ const output = (str, regex, target) => {
   target.innerHTML = str.replace(regex, str => `<span>${str}</span>`);
 };
 
-const str = "Is this is me";
-const regex = new RegExp("is");
-const regexlit = /is/gi;
-
-//console.log(regex.test(str));
-//console.log(regex.exec(str));
-
-//console.log(regexlit.test(str));
-console.log(regexlit.exec(str));
-console.log(regexlit.exec(str));
-console.log(regexlit.exec(str));
-
-console.log(str.match(regexlit));
-console.log(str.replace(regexlit, str => "XX"));
-
-console.log(str.search(regexlit));
+const str = `Cat
+sat
+on
+hat.`;
+const regexlit = /\./g;
 
 output(str, regexlit, document.querySelector("pre"));
+
+// const regexlit = /is/gi;
+
+// console.log(regex.test(str));
+// console.log(regex.exec(str));
+
+// //console.log(regexlit.test(str));
+// console.log(regexlit.exec(str));
+// console.log(regexlit.exec(str));
+// console.log(regexlit.exec(str));
+
+// console.log(str.match(regexlit));
+// console.log(str.replace(regexlit, str => "XX"));
+
+// console.log(str.search(regexlit));
